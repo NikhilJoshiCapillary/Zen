49e2b1fbdec861642b210e509b05243f2fd0951d
Nikhil_Joshi <nikhil.joshi@capillary.com>
2024-02-06 14:06:09 +0530

    Few test cases added

--- src/App.test.js
+++ src/App.test.js
@@ -11,4 +11,63 @@ import globalReducer, { initial_state } from './Reducers/globalReducer';
 import { CREATE_TASK, REMOVE_TASK } from './Actions/constant';
 import About from './About';
 
-test.todo("To be done next")
+function initialProps(){
+  render(<Provider store={store}><Todos/></Provider>);
+}
+
+
+describe("All about home page", ()=>{
+  test(("check for all ui-elements"), ()=>{
+    initialProps();
+    const getInputBar = screen.getByTestId("input-bar")
+    const addingBtn = screen.getByTestId("add-btn")
+    expect(getInputBar).toBeInTheDocument()
+    expect(addingBtn).toBeInTheDocument()
+  })
+
+
+
+  test("testing addition of a task", ()=>{
+    initialProps();
+    const inputBar = screen.getByTestId("input-bar");
+    const addingBtn = screen.getByTestId("add-btn");
+    fireEvent.change(inputBar, {target:{
+      value:"Go to Park"
+    }})
+    fireEvent.click(addingBtn);
+    expect(screen.getByText("Go to Park")).toBeInTheDocument();
+    const removeBtn = screen.getByTestId("remove-btn");
+    fireEvent.click(removeBtn)
+  })
+
+
+  test("testing removal of a task", ()=>{
+    initialProps();
+    const inputBar = screen.getByTestId("input-bar");
+    const addingBtn = screen.getByTestId("add-btn");
+    fireEvent.change(inputBar, {target:{
+      value:"Go to Park"
+    }})
+    fireEvent.click(addingBtn);
+    const text = screen.queryByText("Go to Park");
+    const removeBtn = screen.getByTestId("remove-btn");
+    fireEvent.click(removeBtn);
+    expect(text).not.toBeInTheDocument()
+  })
+
+
+  test("testing editing  of a task", ()=>{
+    initialProps();
+    const inputBar = screen.getByTestId("input-bar");
+    const addingBtn = screen.getByTestId("add-btn");
+    fireEvent.change(inputBar, {target:{
+      value:"Go to Park"
+    }})
+    fireEvent.click(addingBtn);
+    const text = screen.queryByText("Go to Park");
+    const removeBtn = screen.getByTestId("remove-btn");
+    fireEvent.click(removeBtn);
+    expect(text).not.toBeInTheDocument()
+  })
+
+})
